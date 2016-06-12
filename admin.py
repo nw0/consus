e782dde20b15fb791afeb718ee1b6765fae6f22b
Nicholas Sim <nicholassimws@gmail.com>
2016-06-12 22:00:06 +0100

    models: add Location, Item

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location, Item
+
+admin.site.register(Location)
+admin.site.register(Item)
--- models.py
+++ models.py
@@ -1,5 +1,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.Model):
+    name        = models.CharField(max_length=20)
+    comment     = models.CharField(max_length=40)
+    parent      = models.ForeignKey('self', default=None, null=True)
+    owner       = models.ForeignKey(User)
+    last_mod    = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s (%d)" % (self.name, self.id)
+
+class Item(models.Model):
+    name        = models.CharField(max_length=20)
+    item_type   = models.CharField(max_length=20)
+    comment     = models.CharField(max_length=40)
+    location    = models.ForeignKey(Location)
+    owner       = models.ForeignKey(User)
+    last_mod    = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s (%s) #%d" % (self.name, self.item_type, self.id)
