c26b8e5ae33b16cc9873cedb908088672e7b889e
Nicholas Sim <nicholassimws@gmail.com>
2016-06-12 22:17:42 +0100

    views: require group access

--- admin.py
+++ admin.py
@@ -4,3 +4,6 @@ from .models import Location, Item
 
 admin.site.register(Location)
 admin.site.register(Item)
+
+def other_checks(user):
+    return user.groups.filter(name="Consus Users")
--- views.py
+++ views.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import generic
 
+from .admin import other_checks
 from .models import Location, Item
 
+decs = [ login_required, user_passes_test(other_checks), ]
+
+@method_decorator(decs, name='dispatch')
 class LocationList(generic.ListView):
     def get_queryset(self):
         return Location.objects.filter(owner=self.request.user).order_by("id")
 
+@method_decorator(decs, name='dispatch')
 class ItemList(generic.ListView):
     def get_queryset(self):
         return Item.objects.filter(owner=self.request.user).order_by("id")
